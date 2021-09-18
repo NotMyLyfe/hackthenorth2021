04fee6a7f698962d287c705f71ca4fdd5bc3b9f4
aarmaane <aarmaane@gmail.com>
2021-09-18 17:37:11 -0400

    Started routing

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,32 @@
 const router = require('express').Router();
 
 module.exports = router;
+
+// GET methods
+
+router.get("/", function(req, res){
+    if(req.body == "configFile"){
+
+    }
+})
+
+router.get("/mode", function(req, res){
+    if(req.body == "currentMode"){
+
+    }
+    else if(req.body == "allModes"){
+
+    }
+    else if(req.body == "modeConfig"){
+        // Read requested config from paramaters
+        requestedConfig = req.params.mode
+
+    }
+})
+
+router.get("/discord", function(req, res){
+    if(req.body == "userInfo"){
+        requestedUser = req.params.userID
+
+    }
+})
