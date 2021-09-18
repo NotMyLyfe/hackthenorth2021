f42e699dd62088e94578e99d394ab1099607fa70
sa35577 <s97arora@uwaterloo.ca>
2021-09-18 13:08:27 -0400

    sms and general vue page base templaets

--- src/router/index.js
+++ src/router/index.js
@@ -17,7 +17,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path : '/sms',
+    name : 'SMS',
+    component: () => import('../views/SMS.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/SMS.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="mainPage">
+    <a-page-header
+      title="SMS Settings"
+      sub-title = "Local SMS setup - all you!"
+
+    />
+    <a-row class = "rowGap">
+      <a-col>
+        API Key: 
+        <a-input placeholder="Enter a valid API key" />
+      </a-col>
+    </a-row>
+    <a-row class = "rowGap">
+      <a-col>
+        Sending Phone Number: 
+        <a-input placeholder="Don't mess up the number!" />
+      </a-col>
+    </a-row>
+    <a-row class = "rowGap">
+      <a-col>
+        Receiving Phone Number: 
+        <a-input placeholder="Enter your number (or prank a friend)" />
+      </a-col>
+    </a-row>
+    <a-row type = "flex" justify="end" id = "configRow">
+        <a-col>
+          <a-button type="danger">Cancel</a-button>
+          &nbsp;
+          <a-button type="primary">Save Changes</a-button>
+        </a-col>
+        
+      </a-row> 
+  </div>
+  
+</template>
+<style scoped>
+  #mainPage {
+    margin : 5vh 2.5vw;
+  }
+  #configRow {
+    margin-top: 1vh;
+  }
+  .rowGap {
+    margin-top: 0.5vh;
+    margin-bottom : 0.5vh;
+  }
+</style>
+<script>
+
+export default {
+  data() {
+    return {
+    
+    }
+  },
+  methods : {
+    
+  }
+}
+</script>
+
